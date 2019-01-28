160a1be535119877152144c125b7ca4b1d0898a0
dtriglianos <dtriglianos@apelon.com>
2019-01-28 18:54:17 -0500

    Only clear specific folders when running UTGGenerator

--- src/tools/java/org.hl7.fhir.utg/src/org/hl7/fhir/utg/UTGGenerator.java
+++ src/tools/java/org.hl7.fhir.utg/src/org/hl7/fhir/utg/UTGGenerator.java
@@ -142,8 +142,12 @@ public class UTGGenerator extends BaseGenerator {
 
 	private void createMissingOutputFolders() throws IOException {
 		Files.createDirectories(Paths.get(Utilities.path(dest)));
-		// Clear the output folder in case it already existed and contained prior data
-		Utilities.clearDirectory(Utilities.path(dest));
+		// Clear the output folders in case any already existed and contained prior data
+		Utilities.clearDirectory(Utilities.path(dest, FolderNameConstants.UNIFIED));
+		Utilities.clearDirectory(Utilities.path(dest, FolderNameConstants.RELEASE));
+		Utilities.clearDirectory(Utilities.path(dest, FolderNameConstants.EXTERNAL));
+		Utilities.clearDirectory(Utilities.path(dest, FolderNameConstants.V2));
+		Utilities.clearDirectory(Utilities.path(dest, FolderNameConstants.V3));
 		Files.createDirectories(Paths.get(Utilities.path(dest, FolderNameConstants.UNIFIED)));
 		Files.createDirectories(Paths.get(Utilities.path(dest, FolderNameConstants.RELEASE)));
 		Files.createDirectories(Paths.get(Utilities.path(dest, FolderNameConstants.EXTERNAL)));
